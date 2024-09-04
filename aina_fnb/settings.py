79811d1c05e66653078aa80aee7f2e4dbd267a19
Ezar Surahman <akhdanezar@gmail.com>
2024-09-04 15:45:31 +0700

    add ALLOWED_HOST to deplyoment

--- aina_fnb/settings.py
+++ aina_fnb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-po5@6k*t%ptbht1ewe5@0$(^3eq0p9&lq=aef8e6i5sp7-aq(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','ezar-akhdan-ainafnb.pbp.cs.ui.ac.id']
 
 
 # Application definition
